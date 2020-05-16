95cbaf6f41faab44f15743c267f04707cb44353b
Jim <1150660744@qq.com>
2020-05-16 10:49:17 +0800

    add necessary library
    Signed-off-by: Jim <1150660744@qq.com>

---
+++ buili_lib.sh
@@ -0,0 +1,7 @@
+cd alsa-lib-1.1.8; ./configure; make ;sudo make install;cd ..
+
+cd faad2-2.7;./configure; make ;sudo make install; cd .. 
+
+cd libmad-0.15.1b;./configure; make ; sudo make install; cd ..
+
+cd libsamplerate-0.1.9;./configure; make ; sudo make install; cd ..
